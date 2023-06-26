c76b1bc88a54278bd1fccdcfda439a67d70708f6
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-06-26 23:06:04 +0200

    setup files

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, Events } = require('discord.js');
+const { prefix } = require('./config.json');
+const { execSync } = require('child_process');
+
+const client = new Client({
+
+});
+
+client.on(Events.MessageCreate, async (message) => {
+  if (message.author.bot || !message.content.startsWith(prefix)) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'run') {
+    try {
+      await execSync(args.join(' '));
+      await message.reply({
+        content: 'The command has been successfully ran!'
+      });
+    } catch (e) {
+      await message.reply({
+        content: `An error occurred while running command: ${e.message}`
+      });
+    }
+  }
+});
